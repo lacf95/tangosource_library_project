2e6d4ddb0e15f51e1976e2a7e03082df43291c0a
Luis Adrian Chavez Fregoso <biolacf@gmail.com>
2019-07-16 12:11:09 -0500

    Added Markdown to Book's summary

--- lib/tangosource_library_project_web/views/shared_view.ex
+++ lib/tangosource_library_project_web/views/shared_view.ex
@@ -1,3 +1,10 @@
 defmodule TangosourceLibraryProjectWeb.SharedView do
   use TangosourceLibraryProjectWeb, :view
+  require Earmark
+
+  def markdown_content(content) do
+    content
+    |> Earmark.as_html!
+    |> raw
+  end
 end
--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule TangosourceLibraryProject.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:earmark, "~> 1.3"}
     ]
   end
 
