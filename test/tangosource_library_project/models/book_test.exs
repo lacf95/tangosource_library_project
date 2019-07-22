d557e62d262891b884ac1d8e948c7798451ab4b6
Luis Adrian Chavez Fregoso <biolacf@gmail.com>
2019-07-22 17:36:28 -0500

    Completed Book model and controller tests

--- test/tangosource_library_project/models/book_test.exs
+++ test/tangosource_library_project/models/book_test.exs
@@ -1,9 +1,11 @@
 defmodule TangosourceLibraryProject.BookTest do
   use TangosourceLibraryProject.DataCase
 
+  import TangosourceLibraryProject.BookFixtures
+
   alias TangosourceLibraryProject.Book
 
-  @valid_attributes %{
+  @valid_attrs %{
     title: "Learning the Bash Shell",
     author: "Cameron Newham & Bill Rosenblatt",
     isbn: "978-0596009656",
@@ -12,23 +14,14 @@ defmodule TangosourceLibraryProject.BookTest do
     published_at: ~D[2005-04-01],
     tags: "bash, shell, sh, zsh, command line, terminal"
   }
-  @invalid_attributes %{
-    title: nil,
-    author: nil,
-    isbn: "978-0596009656",
-    edition: 0,
-    summary: "# Learning the Bash Shell summary",
-    published_at: ~D[2005-04-01],
-    tags: "bash, shell, sh, zsh, command line, terminal"
-  }
-  @valid_cover_attributes %{
+  @valid_cover_attrs %{
     cover: %Plug.Upload{
       content_type: "image/jpeg",
       filename: "book_cover.jpg",
       path: "test/fixtures/files/book_cover.jpg"
     }
   }
-  @invalid_cover_attributes %{
+  @invalid_cover_attrs %{
     cover: %Plug.Upload{
       content_type: "text/plain",
       filename: "invalid_book_cover.file",
@@ -36,23 +29,65 @@ defmodule TangosourceLibraryProject.BookTest do
     }
   }
 
-  test "changeset with valid attributes" do
-    changeset = Book.changeset(%Book{}, @valid_attributes)
+  test "changeset with valid attrs" do
+    changeset = Book.changeset(%Book{}, @valid_attrs)
     assert changeset.valid?
   end
 
-  test "changeset with invalid attributes" do
-    changeset = Book.changeset(%Book{}, @invalid_attributes)
+  test "changeset with no title" do
+    attrs = @valid_attrs |> Map.put(:title, nil)
+    changeset = Book.changeset(%Book{}, attrs)
+    refute changeset.valid?
+  end
+
+  test "changeset with no author" do
+    attrs = @valid_attrs |> Map.put(:author, nil)
+    changeset = Book.changeset(%Book{}, attrs)
+    refute changeset.valid?
+  end
+
+  test "changeset with invalid edition" do
+    attrs = @valid_attrs |> Map.put(:edition, 0)
+    changeset = Book.changeset(%Book{}, attrs)
     refute changeset.valid?
   end
 
   test "book is created with valid cover" do
-    attributes = @valid_attributes |> Map.merge(@valid_cover_attributes)
-    assert {:ok, %Book{}} = Book.create(attributes)
+    attrs = @valid_attrs |> Map.merge(@valid_cover_attrs)
+    assert {:ok, %Book{}} = Book.create(attrs)
   end
 
   test "book is created with invalid cover" do
-    attributes = @valid_attributes |> Map.merge(@invalid_cover_attributes)
-    assert {:error, changeset} = Book.create(attributes)
+    attrs = @valid_attrs |> Map.merge(@invalid_cover_attrs)
+    assert {:error, changeset} = Book.create(attrs)
+  end
+
+  test "book is updated with valid attrs" do
+    book = fixture(:book)
+    attrs = @valid_attrs |> Map.put(:edition, 1)
+    assert {:ok, %Book{}} = Book.update(book, attrs)
+  end
+
+  test "book is not updated with invalid author" do
+    book = fixture(:book)
+    attrs = @valid_attrs |> Map.put(:author, nil)
+    assert {:error, changeset} = Book.update(book, attrs)
+  end
+
+  test "book is not updated with invalid title" do
+    book = fixture(:book)
+    attrs = @valid_attrs |> Map.put(:title, nil)
+    assert {:error, changeset} = Book.update(book, attrs)
+  end
+
+  test "book is deleted" do
+    book = fixture(:book)
+    assert {:ok, book} = Book.delete(book)
+  end
+
+  test "book is found" do
+    book = fixture(:book)
+    found_book = Book.find!(book.id)
+    assert book = found_book
   end
 end
--- test/tangosource_library_project_web/controllers/books_controller_test.exs
+++ test/tangosource_library_project_web/controllers/books_controller_test.exs
@@ -1,13 +1,87 @@
 defmodule TangosourceLibraryProjectWeb.BooksControllerTest do
   use TangosourceLibraryProjectWeb.ConnCase
 
+  import TangosourceLibraryProject.BookFixtures
+
   test "GET /", %{conn: conn} do
+    book = fixture(:book)
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "The TangoSource Library Project"
+    assert html_response(conn, 200) =~ "Share a Book With Us"
+    assert html_response(conn, 200) =~ book.title
   end
 
   test "GET /books", %{conn: conn} do
+    book = fixture(:book)
     conn = get(conn, "/books")
     assert html_response(conn, 200) =~ "The TangoSource Library Project"
+    assert html_response(conn, 200) =~ "Share a Book With Us"
+    assert html_response(conn, 200) =~ book.title
+  end
+
+  test "GET /books/:id", %{conn: conn} do
+    book = fixture(:book)
+    conn = get(conn, "/books/#{book.id}")
+    assert html_response(conn, 200) =~ book.title
+    assert html_response(conn, 200) =~ "Edit This Book"
+  end
+
+  test "GET /books/:id/edit", %{conn: conn} do
+    book = fixture(:book)
+    conn = get(conn, "/books/#{book.id}/edit")
+    assert html_response(conn, 200) =~ "Edit #{book.title}"
+  end
+
+  test "POST /books", %{conn: conn} do
+    conn = post(conn, "/books", book: %{
+      title: "Learning the Bash Shell",
+      author: "Cameron Newham & Bill Rosenblatt",
+      isbn: "978-0596009656",
+      edition: 3,
+      summary: "# Learning the Bash Shell summary",
+      published_at: ~D[2005-04-01],
+      tags: "bash, shell, sh, zsh, command line, terminal"
+    })
+
+    assert html_response(conn, 302)
+  end
+
+  test "POST /books Invalid", %{conn: conn} do
+    conn = post(conn, "/books", book: %{
+      title: nil,
+      author: "Cameron Newham & Bill Rosenblatt",
+      isbn: "978-0596009656",
+      edition: 3,
+      summary: "# Learning the Bash Shell summary",
+      published_at: ~D[2005-04-01],
+      tags: "bash, shell, sh, zsh, command line, terminal"
+    })
+
+    assert html_response(conn, 200) =~ "can&#39;t be blank"
+  end
+
+  test "PUT /books/:id", %{conn: conn} do
+    book = fixture(:book)
+    conn = put(conn, "/books/#{book.id}", book: %{
+      title: "Learning the Super Bash Shell"
+    })
+
+    assert html_response(conn, 302)
+  end
+
+  test "PUT /books/:id Invalid", %{conn: conn} do
+    book = fixture(:book)
+    conn = put(conn, "/books/#{book.id}", book: %{
+      title: nil
+    })
+
+    assert html_response(conn, 200) =~ "can&#39;t be blank"
+  end
+
+  test "DELETE /books/:id", %{conn: conn} do
+    book = fixture(:book)
+    conn = delete(conn, "/books/#{book.id}")
+
+    assert html_response(conn, 302)
   end
 end
